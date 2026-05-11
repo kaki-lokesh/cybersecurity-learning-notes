a7bae7aa6183861b733e1b4b61d20c28a146dcef
kaki-lokesh <lkaki@student.gitam.edu>
2026-05-11 18:13:25 +0000

    day 1: linux terminal notes

---
+++ week1/day1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Day 1 - Linux commands I learned today
+
+# NAVIGATION COMMANDS
+# 1. pwd : Shows which directory I am in currently.
+
+# 2. ls : List directory contents
+# 2.1. ls -l : Gives detailed list
+# 2.2. ls -la : Shows hidden files also
+
+# 3. cd : Change directory
+# 3.1. cd ~ : Goes to your home directory from anywhere
+# 3.2. cd .. : Goes up ONE directory level
+# 3.3. cd - : Goes back to the PREVIOUS directory
+
+# CREATING AND MANAGING FILES
+# 1. mkdir : Make Directory
+# 2. touch : Create an empty file
+# 3. echo : Print text OR redirect text into a file
+# 3.1. > : OVERWRITES existing content in the file
+# 3.2. >> : Adds contents at the last in the file
+# 4. cat : Display file contents
+# 5. less : View large files one page at a time (press q to quit)
+# 6. head and tail : First N or Last N lines of a file
+# 7. cp : Copy a file
+# 9. rm : Remove/delete files
+# 10. find : Search for files by name, size, date, permissions
