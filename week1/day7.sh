504619a79f7b40f481c80efee97c7a62c1df3aaa
kaki-lokesh <lkaki@student.gitam.edu>
2026-05-17 22:14:35 +0530

    day7: SSH brute protection tool

---
+++ week1/day7.sh
@@ -0,0 +1,10 @@
+#!/bib/bash 
+
+# SSH BRUTE PROTECTION TOOL
+  This python script
+  1. reads an auth.log file
+  2. extracts all failed SSH login attempts
+  3. identifies potential brute-force attackers (IPs with more than Nattempts) 
+  4. generates a formatted security report
+
+The complete 6 days learnt knowledge is combined to develop this python security tool that does real work.	
