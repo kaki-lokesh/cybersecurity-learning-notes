8de19a1e10403b26e9ebdfd3634462ea33fd8861
kaki-lokesh <lkaki@student.gitam.edu>
2026-05-24 21:09:13 +0530

    day14: TCP port scanner tool

---
+++ week2/day14.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TCP PORT SCANNER WITH BANNER GRABBING
+  This python CLI tool:
+  1. scans a target IP across a port range
+  2. identifies open ports
+  3. grabs service banners
+  4. identifies services from port numbers and banner text
+  5. generates a formatted report
+
+The complete 6 days learnt knowledge is combined to develop this python CLI tool that replicates core Nmap functionality.
+
