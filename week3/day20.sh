cffe598f07b3591e85f32c0cd32358e9253eaa87
kaki-lokesh <lkaki@student.gitam.edu>
2026-05-30 16:32:10 +0000

    day20: subprocess

---
+++ week3/day20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# SUBPROCESS
+  The subprocess module runs external programs from Python code and captures their output.
+  We can drive Nmap, ffuf, sqlmap, or any command-line utility from Python, capture its output, parse it, and take automated action based on the results.
+  This is the foundation of security automation.
+
+  # This concept is practiced in python file: subprocess_masterclass.py
