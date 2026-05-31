72630fc98ebc6543355a38335ff50738ec40dfce
kaki-lokesh <lkaki@student.gitam.edu>
2026-05-31 17:32:18 +0000

    day21: Complete revision of this week

---
+++ week3/day21.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+" Revise all the week progress - 5 python security tools with concepts; run the tool files and fix issues encountered "
+" Completed Google Cybersecurity - Course 1: Foundations of Cybersecurity from Coursera "
