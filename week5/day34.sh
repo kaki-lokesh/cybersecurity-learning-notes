879fcc7d6a2a4d92e123b090b087c5bfbc9cb89f
kaki-lokesh <lkaki@student.gitam.edu>
2026-06-13 22:47:01 +0530

    day34: HTB Jerry Writeup

---
+++ week5/day34.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# HTB JERRY WRITEUP
+  A CTF/HTB writeup is not just documentation - it is a demonstration of your thinking process.
+  A writeup must show:
+  - Methodical Approach (not just lucky guesses)
+  - Understanding of why each step worked
+  - Security Implications
+  - Remediation Recommendations
+
+  # Writeup: jerry.md
