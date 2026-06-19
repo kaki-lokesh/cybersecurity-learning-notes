47517a36c3df8bd638774c0723e1796e607252ef
kaki-lokesh <lkaki@student.gitam.edu>
2026-06-19 22:06:40 +0530

    day40: Report Generation

---
+++ week6/day40.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# REPORT GENERATION
+  A threat intelligence report must answer three questions for its consumer:
+  - What is this thing? (identification)
+  - Is it bad? (risk assessment)
+  - Why do you think that? (evidence)
